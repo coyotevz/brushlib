edc5d3bced326f64aa7258b020d83b883ae3207f
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-12-24 20:33:31 -0300

    More object in objects model

--- brushlib/objects_model.py
+++ brushlib/objects_model.py
@@ -9,6 +9,23 @@ class PenInfo(object):
     ytilt = 0
     dtime = 0
 
+class BrushSetting(object):
+
+    cname = None
+    name = None
+    constant = None
+    min = 0
+    default = 0
+    max = 0
+    tooltip = None
+
+    def get_name(self):
+        pass
+
+    def get_tooltip(self):
+        pass
+
+
 class Brush(object):
 
     def new(self):
@@ -63,4 +80,70 @@ class Brush(object):
         pass
 
     def from_string(self, string: const char):
+        pass
+
+class Mapping(object):
+
+    def get_base_value(self):
+        pass
+
+    def set_base_value(self, value):
+        pass
+
+    def get_n(self, input: int):
+        pass
+
+    def set_n(self, input: int, n: int):
+        pass
+
+    def set_point(self, input: int, index: int, x, y):
+        pass
+
+    def get_point(self, input: int, index: int, x, y):
+        pass
+
+    def is_constant(self):
+        pass
+
+    def get_input_used_n(self):
+        pass
+
+    def calculate(self, data):
+        pass
+
+    def calculate_single_input(self, input):
+        pass
+
+class Surface(object):
+    
+    def draw_dab(self, x, y, radius, color_r, color_g, color_b, opaque, hardness,
+                 alpha_eraser, aspect_ratio, angle, lock_alpha, colorize):
+        pass
+
+    def get_color(self, x, y, radius, color_r: out, color_g: out, color_b: out,
+                  color_a: out):
+        pass
+
+    def get_alpha(self, x, y, radius):
+        pass
+
+    def save_png(self, path, x, y, width, height):
+        pass
+
+    def begin_atomic(self):
+        pass
+
+    def end_atomic(self):
+        pass
+
+
+class TiledSurface(Surface):
+
+    def set_symmetry_state(self, active, center_x):
+        pass
+
+    def tile_request_start(self, request: TileRequest):
+        pass
+
+    def tile_request_end(self, request: TileRequest):
         pass
