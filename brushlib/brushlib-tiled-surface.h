c9850ff61e01b6e5f6b0f991ce3b42989ddd18f1
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-12-24 20:33:48 -0300

    Start writing tiled-surface code

---
+++ brushlib/brushlib-tiled-surface.h
@@ -0,0 +1,52 @@
+/*
+ * BrushLib - The MyPaint Brush Library
+ *
+ * Copyright (C) 2007-2014 Martin Renold <martinxyz@gmx.ch> et. al.
+ * Copyright (C) 2014 Augusto Roccasalva <augusto@rioplomo.com.ar>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef __BRUSHLIB_TILED_SURFACE_H__
+#define __BRUSHLIB_TILED_SURFACE_H__
+
+#if !defined(__BRUSHLIB_H_INSIDE__) && !defined (BRUSHLIB_COMPILATION)
+#error "Only <brushlib/brushlib.h> can be included directly."
+#endif
+
+#include <glib-gobject.h>
+#include <brushlib/brushlib-types.h>
+#include <brushlib/brushlib-surface.h>
+
+G_BEGIN_DECLS
+
+#define BRUSHLIB_TYPE_TILED_SURFACE \
+  (brushlib_surface_get_type ())
+#define BRUSHLIB_TILED_SURFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), BRUSHLIB_TYPE_TILED_SURFACE, BrushLibTiledSurface))
+#define BRUSHLIB_TILED_SURFACE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), BRUSHLIB_TYPE_TILED_SURFACE, BrushLibTiledSurfaceClass))
+#define BRUSHLIB_IS_TILED_SURFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), BRUSHLIB_TYPE_TILED_SURFACE))
+#define BRUSHLIB_IS_TILED_SURFACE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), BRUSHLIB_TYPE_TILED_SURFACE))
+#define BRUSHLIB_TILED_SURFACE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), BRUSHLIB_TYPE_TILED_SURFACE, BrushLibTiledSurfaceClass))
+
+/* BrushLibTiledSurface alias is defined in brushlib-types.h */
+typedef struct _BrushLibTiledSurfaceClass  BrushLibTiledSurfaceClass;
+
+G_END_DECLS
+
+#endif /* __BRUSHLIB_TILED_SURFACE_H__ */
+
